84af68bc27feb04bae8fdcebf9319af5bc623846
yurisr123 <48933033+yurisr123@users.noreply.github.com>
2026-05-27 00:13:47 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
-function Home(){
-    return <div style={{textAlign: 'center', fontFamily:"cursive"}}>
-                <h1><b>Frase alterada novamente</b></h1>
-            </div>
+function Home() {
+  return (
+    <div style={{ textAlign: "center", fontFamily: "cursive" }}>
+      <h1>
+        <b>Frase alterada novamente</b>
+      </h1>
+    </div>
+  );
 }
 
 export default Home;
